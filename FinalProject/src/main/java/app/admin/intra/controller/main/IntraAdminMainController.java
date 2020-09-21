bdb39eb91e3de725af0c51960f6a7ace2a8bd612
SangMin <tkdals9048@naver.com>
2020-09-21 14:35:18 +0900

    2020 0921 01 - 직원 관리 추가

---
+++ FinalProject/src/main/java/app/admin/intra/controller/main/IntraAdminMainController.java
@@ -0,0 +1,13 @@
+package app.admin.intra.controller.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/intra")
+public class IntraAdminMainController {
+	@RequestMapping("/")
+	public String home() {
+		return "thymeleaf/admin/intra/index";
+	}
+}
